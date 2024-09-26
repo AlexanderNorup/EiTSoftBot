13d04fcdfe2d0e60a88e17459cb67bf6801107fe
Alexander Nørup <me@alexandernorup.com>
2024-09-26 10:20:48 +0200

    Ensure you can't drag the boxes off the MiR

--- EiTSoftBot/wwwroot/js/box.js
+++ EiTSoftBot/wwwroot/js/box.js
@@ -1,60 +1,74 @@
 class Box {
-  id = crypto.randomUUID();
-  dragging = false;
-  mouseOver = false;
-  highlight = false;
-  x = 0;
-  y = 0;
-  z = 0;
-  width = 0;
-  length = 0;
-  height = 0;
-  weight = 0;
-  offsetX = 0;
-  offsetY = 0;
-  /**
-  * @param {number} x
-  * @param {number} y
-  * @param {number} width
-  * @param {number} length
-  * @param {number} height
-  * @param {number} weight
-  */
-  constructor(x, y, width, length, height, weight) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.length = length;
-    this.height = height;
-    this.weight = weight;
-  }
+    id = crypto.randomUUID();
+    dragging = false;
+    mouseOver = false;
+    highlight = false;
+    x = 0;
+    y = 0;
+    z = 0;
+    width = 0;
+    length = 0;
+    height = 0;
+    weight = 0;
+    offsetX = 0;
+    offsetY = 0;
 
-  update(p) {
-    if (this.dragging) {
-      this.x = p.mouseX + this.offsetX;
-      this.y = p.mouseY + this.offsetY;
+    /**
+    * @param {number} x
+    * @param {number} y
+    * @param {number} width
+    * @param {number} length
+    * @param {number} height
+    * @param {number} weight
+    */
+    constructor(x, y, width, length, height, weight) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+        this.weight = weight;
     }
-  }
 
-  show(p) {
-    p.stroke(0);
-    if (this.dragging) {
-      p.fill(50);
-    } else if (this.mouseOver) {
-      p.fill(100);
-    } else if (this.highlight) {
-      p.fill('green');
-    } else {
-      p.fill(150);
+    update(p) {
+        if (this.dragging) {
+            const xOverhang = this.width / 2;
+            const yOverhang = this.length / 2;
+            this.x = clamp(p.mouseX + this.offsetX, 0 - xOverhang, p.width - xOverhang);
+            this.y = clamp(p.mouseY + this.offsetY, 0 - yOverhang, p.height - yOverhang);
+        }
     }
-    p.rect(this.x, this.y, this.width, this.length);
-    p.fill(0);
-    p.stroke(0);
-    p.text(`Z: ${this.z}\nH: ${this.height}`, this.x + 10, this.y + 20);
 
-    if (window.update3DBox !== undefined) {
-      window.update3DBox(this);
+    show(p) {
+        p.stroke(0);
+        if (this.dragging) {
+            p.fill(50);
+        } else if (this.mouseOver) {
+            p.fill(100);
+        } else if (this.highlight) {
+            p.fill('green');
+        } else {
+            p.fill(150);
+        }
+        p.rect(this.x, this.y, this.width, this.length);
+        p.fill(0);
+        p.stroke(0);
+        p.text(`Z: ${this.z}\nH: ${this.height}`, this.x + 10, this.y + 20);
+
+        if (window.update3DBox !== undefined) {
+            window.update3DBox(this);
+        }
     }
-  }
 
+}
+
+/**
+ * Clamps a number between a minimum and maximum value
+ * @param {Number} value
+ * @param {Number} min
+ * @param {Number} max
+ * @returns {Number}
+ */
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
 }
