00397869c157746f4738c9435f8d2f2a3f1c1f91
Alexander Nørup <me@alexandernorup.com>
2024-10-03 11:45:08 +0200

    Added a bit of visual feedback when sending to simulation

---
+++ EiTSoftBot/wwwroot/js/toastInterop.js
@@ -0,0 +1,13 @@
+﻿window.showSuccessToast = (message) => {
+    Toastify({
+        text: message,
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+    }).showToast();
+};
