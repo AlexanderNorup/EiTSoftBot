c24ec6b61b9dd02a109608a52079a4af66c5f9ce
Alexander Nørup <me@alexandernorup.com>
2024-11-14 09:17:10 +0100

    Set TypeNameHandling to Objects

--- EiTSoftBot.Dto/RequestSerializer.cs
+++ EiTSoftBot.Dto/RequestSerializer.cs
@@ -6,7 +6,7 @@ namespace EiTSoftBot.Dto
     {
         private static JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
         {
-            TypeNameHandling = TypeNameHandling.Auto,
+            TypeNameHandling = TypeNameHandling.Objects,
         };
 
         public static string Serialize(object request)
